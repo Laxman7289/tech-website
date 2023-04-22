347d2f65c39b0690afd2a64a4259d00d67db3b6a
Laxman7289 <127031359+Laxman7289@users.noreply.github.com>
2023-04-22 12:15:05 +0530

    add

--- src/Card.js
+++ src/Card.js
@@ -13,7 +13,7 @@ function Card(props) {
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <NavLink to="" className="btn btn-primary">Go somewhere</NavLink>
+                        <NavLink to="" className="btn btn-primary nav-link">Go somewhere</NavLink>
                     </div>
                 </div>
             </div>
